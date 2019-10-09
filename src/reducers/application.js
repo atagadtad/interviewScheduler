0b722ec5163275a46cce7ff1495e1e75acffb306
Aaron Tagadtad <aarontagadtad@Aaron-Tagadtads-MacBook-Pro.local>
2019-10-09 16:00:13 -0400

    following example to break code for circle ci

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -5,11 +5,11 @@ export const DELETE_INTERVIEW = "DELETE_INTERVIEW";
 
 export default function reducer(state, action) {
   switch (action.type) {
-    case SET_DAY:
-      return {
-        ...state,
-        day: action.day
-      };
+    // case SET_DAY:
+    //   return {
+    //     ...state,
+    //     day: action.day
+    //   };
     case SET_APPLICATION_DATA:
       return {
         ...state,
